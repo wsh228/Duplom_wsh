5a267ef8e78643c45ecb69b35edea9de47b3a504
wsh228 <49440712+wsh228@users.noreply.github.com>
2019-04-09 14:19:47 +0200

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write first number: ");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Write operation: ");
+            char operation = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Write second number: ");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Answer");
+
+            int answer = 0;
+            switch (operation)
+            {
+                case '+':
+                    answer = firstNum + secondNum;
+                    break;
+                case '-':
+                    answer = firstNum - secondNum;
+                    break;
+                case '*':
+                    answer = firstNum * secondNum;
+                    break;
+                case '/':
+                    answer = firstNum / secondNum;
+                    break;
+                case '%':
+                    answer = firstNum % secondNum;
+                    break;
+                case '^':
+                    answer = 1;
+                    for(int i = 0; i < secondNum; i++)
+                    {
+                        answer *= firstNum;
+                    }
+                    break;
+            }
+            Console.WriteLine(answer);
+            Console.ReadKey();
+        }
+    }
+}
